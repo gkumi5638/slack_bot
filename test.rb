beb74c4ec3c295d101bd52beff23009f8284215a
kumi <>
2018-03-31 00:22:14 +0900

    Authentication,トークンの設定

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 require 'http'
 require 'json'
 
-response = HTTP.post("https://slack.com/api/api.test")
+response = HTTP.post("https://slack.com/api/auth.test", params: {token: ENV['SLACK_API_TOKEN']})
 puts JSON.pretty_generate(JSON.parse(response.body))
