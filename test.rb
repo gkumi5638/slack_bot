5a3f079d540f9f352f1aadbf601d51a3373861c6
kumi <>
2018-04-01 13:08:50 +0900

    1.3. Post Message

--- test.rb
+++ test.rb
@@ -1,5 +1,10 @@
 require 'http'
 require 'json'
 
-response = HTTP.post("https://slack.com/api/auth.test", params: {token: ENV['SLACK_API_TOKEN']})
+response = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+    token: ENV['SLACK_API_TOKEN'],
+    channel: "#random",
+    text: "こんにちは!",
+    as_user: true,
+  })
 puts JSON.pretty_generate(JSON.parse(response.body))
